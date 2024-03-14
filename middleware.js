fd8ee9e1f1c1b81cdea54ad174f0f949588d00b2
Aryan0404 <aryanpoojary04@gmail.com>
2024-03-14 23:28:17 +0530

    Adding authorization

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const { JournalEntry } = require("./models/journal");
+const { Review } = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -14,7 +15,30 @@ module.exports.storeReturnTo = (req, res, next) => {
   }
   next();
 };
-module.exports.isAuthor = async (req, res, next) => {
-  const { id } = req.params;
-  const review = await JournalEntry.findById(id);
+// module.exports.isAuthor = async (req, res, next) => {
+//   const { id } = req.params;
+//   const review = await JournalEntry.findById(id);
+//   if(!review.authors.equal(req.user._id)){
+//     return res.
+//   }
+// };
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    //throw new ExpressError(msg,404)
+  } else {
+    next();
+  }
+};
+module.exports.reviewAuthor = async (req, res, next) => {
+  const { id, reviewid } = req.params;
+  console.log(reviewid);
+  const review = await Review.findById(reviewid);
+  console.log(review);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that");
+    // return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
 };
