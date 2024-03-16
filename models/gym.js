2c879a878950fcffbbe8a39bf8209b5f0ad641c1
Aryan0404 <aryanpoojary04@gmail.com>
2024-03-16 19:17:48 +0530

    Added some gym page

--- models/gym.js
+++ models/gym.js
@@ -1,48 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const User = require("./user");
 const imageSchema = new Schema({
-    url: {
-      type: String,
-      required: true,
-    },
-    filename: {
-      type: String,
-      required: true,
-    },
-  });
+  url: {
+    type: String,
+    required: true,
+  },
+  filename: {
+    type: String,
+    required: true,
+  },
+});
 
 const gymSchema = new Schema({
-    name : {
-        type : String,
-        required : true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  images: [imageSchema],
+  price: {
+    type: Number,
+    required: true,
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    description : {
-        type : String,
-        required : true
-    },
-    location : {
-        type : String,
-        required : true
-    },
-    images: [imageSchema],
-    price : {
-        type: Number,
-        required : true
-    },
-    reviews : [
-        {
-            type : Schema.Types.ObjectId,
-            ref : "Review"
-        }
-    ],
-    author : {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: User,
+  },
 });
 
 const Gym = mongoose.model("Gym", gymSchema);
 
-module.exports = { Gym }
+module.exports = { Gym };
